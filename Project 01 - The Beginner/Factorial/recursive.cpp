44ce605780d1b2471f1a8da0d20f975fcf17ac21
Alp 'Lulu <87658293+luluwux@users.noreply.github.com>
2025-11-04 13:41:39 +0300

    Implement recursive factorial function
    Added a recursive function to calculate factorial and updated main to display the result.

--- Project 01 - The Beginner/Factorial/recursive.cpp
+++ Project 01 - The Beginner/Factorial/recursive.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
-  // Recursive function to calculate factorial
-int main() {
-    int number; 
-    cout << "Enter a non-negative integer: ";
-    cin >> number;
 
-    // Devam edecek
+int factorial(int n) {
+  if (n > 1) {
+    return n * factorial(n - 1);
+  } else {
+    return 1;
+  }
+}
+
+int main() {
+  int number;
+  cout << "Enter a non-negative integer: "; cin >> number;
+  cout << "Result: " << factorial(number) << endl;
+  return 0;
 }
